2f3c98b488182ce671047aba3e2f41b794ff7498
TsaoLun <ycl18@live.com>
2023-05-05 01:08:07 +0800

    modified:   .vscode/settings.json 	modified:   py/ch4_02.py

--- py/ch4_02.py
+++ py/ch4_02.py
@@ -1,22 +1,41 @@
 import numpy as np
 import matplotlib.pylab as pylab
+
 # import matplotlib.pyplot as plt
 import unittest
+
 # from mpl_toolkits.mplot3d import axes3d
 # 数值微分（求导）
 
 
 def numerical_diff(f, x):
     h = 1e-4
-    return (f(x+h) - f(x-h)) / (2*h)
+    return (f(x + h) - f(x - h)) / (2 * h)
 
 
 def function_1(x):
-    return 0.01*x**2 + 0.1*x
+    return 0.01 * x**2 + 0.1 * x
 
 
 def function_2(x):
-    return x[0]**2 + x[1]**2
+    return x[0] ** 2 + x[1] ** 2
+
+
+# 梯度计算
+def numerical_gradient(f, x: np.ndarray):
+    h = 1e-4
+    grad = np.zeros_like(x)  # 生成 x 形状的数组
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+    return grad
 
 
 class DrawDiff(unittest.TestCase):
@@ -29,15 +48,16 @@ class DrawDiff(unittest.TestCase):
         pylab.plot(x, y)
         pylab.show()
 
+    @unittest.skip
     def test_f2(self):
         x0 = np.arange(-3.0, 3.0, 0.1)
         x1 = np.arange(-3.0, 3.0, 0.1)
         y = function_2(np.array([x0, x1]))
-        pylab.xlabel("x")
-        pylab.ylabel("f(x)")
-        pylab.plot(x0, y)
-        # pylab.show()
+        pass
 
+    def test_ng(self):
+        print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+        print(numerical_gradient(function_2, np.array([0.0, 2.0])))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
