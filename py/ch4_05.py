21315f484013a3c42be2c3c2e7d965ba2dfc6011
TsaoLun <ycl18@live.com>
2023-05-18 00:38:02 +0800

    modified:   py/ch4_05.py

--- py/ch4_05.py
+++ py/ch4_05.py
@@ -35,4 +35,35 @@ class TestLayer(unittest.TestCase):
             train_loss_list.append(loss)
 
     def test_epoch(self):
-        pass
+        train_acc_list = []
+        test_acc_list = []
+        # 平均每个 epoch 的重复次数
+        iter_per_epoch = max(train_size / batch_size, 1)
+
+        network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+        for i in range(iters_num):
+            # 获取 mini-batch
+            batch_mask = np.random.choice(train_size, batch_size)
+            x_batch = x_train[batch_mask]
+            t_batch = t_train[batch_mask]
+
+            # 计算梯度
+            grad = network.numerical_gradient(x_batch, t_batch)
+
+            for key in ("W1", "b1", "W2", "b2"):
+                network.params[key] -= learning_rate * grad[key]
+
+            loss = network.loss(x_batch, t_batch)
+            train_loss_list.append(loss)
+            # 计算每个 epoch 的识别精度
+            if i % iter_per_epoch == 0:
+                train_acc = network.accuracy(x_train, t_train)
+                test_acc = network.accuracy(x_test, t_test)
+                train_acc_list.append(train_acc)
+                test_acc_list.append(test_acc)
+                print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+
+if __name__ == "__main__":
+    unittest.main()
