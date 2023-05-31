09e2fdc069c3c6a231b54e293e8fb50a0805f3f8
TsaoLun <ycl18@live.com>
2023-06-01 00:41:16 +0800

    modified:   py/ch5_01.py

--- py/ch5_01.py
+++ py/ch5_01.py
@@ -1,19 +1,24 @@
+import unittest
+import numpy as np
+
+
 # 乘法层 MulLayer
 class MulLayer:
     def __init__(self):
         self.x = None
         self.y = None
-    
+
     def forward(self, x, y):
         self.x = x
         self.y = y
         out = x * y
         return out
-    
+
     def backend(self, dout):
         dx = dout * self.y
         dy = dout * self.x
         return dx, dy
+
 
 class AddLayer:
     def __init__(self):
@@ -22,9 +27,50 @@ class AddLayer:
     def forward(self, x, y):
         out = x + y
         return out
-    
+
     def backend(self, dout):
         dx = dout
         dy = dout
         return dx, dy
+
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = x <= 0
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+class TestRelu(unittest.TestCase):
+    @unittest.skip
+    def test_relu(self):
+        x = np.array([[1.0, -0.5], [-2.0, 3.0]])
+        print(x)
+        mask = x <= 0
+        print(mask)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
     
+    def forward(self, x):
+        out = 1 / (1+np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
